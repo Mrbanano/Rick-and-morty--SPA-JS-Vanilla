ba61a75bc8ab7c9fbbb2a5de283d1c798594fd9a
Alvaro castillo <6666alvaro666@gmail.com>
2021-07-12 11:33:39 -0500

    :wrench: config webpack

---
+++ src/index.js
@@ -0,0 +1,2 @@
+console.log('hola')
+alert('aqui si jala esta madre ')
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,66 @@
-const path = requiere('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const HtmlwebPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require('copy-webpack-plugin');
+const CssMinizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
-module.exports ={
-    entry:'./src/index.js',
-    output:{
-        path: path.resolve(__dirname,'dist'),
-        filename:'main.js'
-    },
-    resolve:{
-        extensions: ['js'],
-    },
-    module: { 
-        rules: [    
-            {   
-                test: /\,js?$/, 
-                exclude: /node_modules/,  
-                use: {
-                    loader: 'babel-loader', 
-                }
-            }
-        ]
-    },
-    plugins: [  
-        new HtmlWebpackPlugin([   
-            {
-                inject: true,  
-                template: './public/index.html',  
-                filename: './index.html'    
-            }
-        ])
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/images/[hash][ext][query]'
+  },
+  resolve: {
+    extensions: [".js"],
+  }, 
+  module:{
+      rules:[
+          {
+              test: /\.m?js$/,
+              exclude: /node_modules/,
+              use:{
+                  loader:'babel-loader'
+              }
+          },
+          {
+            test: /\.css$/i,
+            use:[
+              MiniCssExtractPlugin.loader,
+              'css-loader',
+              
+            ]
+          },
+          {
+            test: /\.png$/,
+            type:'asset/resource'
+          },
+      ]
+  },
+  plugins:[
+      new HtmlwebPlugin({
+          inject: "body",
+          template:'./public/index.html',
+          filename:'./index.html'
+      }),
+      new MiniCssExtractPlugin({
+        filename:'assets/[name].[contenthash].css'
+      }),
+  ],
+  optimization:{
+    minimize: true,
+    minimizer:[
+      new TerserPlugin(),
+      new CssMinizerPlugin(),
     ]
-}
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 9000,
+    bonjour: true,
+  },
+};
